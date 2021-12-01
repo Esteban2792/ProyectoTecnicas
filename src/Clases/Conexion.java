7d3eb58a3e418db5e82d648b9aedbc92fd436c27
Esteban2792 <85144202+Esteban2792@users.noreply.github.com>
2021-11-30 20:57:17 -0600

    Commit3
    Cambios en el Programa

---
+++ src/Clases/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.sql.*;
+
+public class Conexion {
+    
+    public static Connection ConectaBaseDatos(){
+        String url="jdbc:sqlserver://localhost:49963;"
+                +"database=pruebaUno;"
+                +"user=sa;"
+                +"password=1234;";
+        
+        try{
+            
+        Connection conecta = DriverManager.getConnection(url);
+        return conecta;
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+            return null;
+        } 
+    }
+    
+}
